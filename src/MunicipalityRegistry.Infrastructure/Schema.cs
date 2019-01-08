64fa40bb080d10fca4183e5b18e747467603707f
David Cumps <david@cumps.be>
2019-01-08 13:37:53 +0100

    refactor: remove oslo constants for now

--- src/MunicipalityRegistry.Infrastructure/Schema.cs
+++ src/MunicipalityRegistry.Infrastructure/Schema.cs
@@ -6,13 +6,11 @@ namespace MunicipalityRegistry.Infrastructure
 
         public const string Extract = "MunicipalityRegistryExtract";
         public const string Legacy = "MunicipalityRegistryLegacy";
-        public const string Oslo = "MunicipalityRegistryOslo";
     }
 
     public class MigrationTables
     {
         public const string Extract = "__EFMigrationsHistoryExtract";
         public const string Legacy = "__EFMigrationsHistoryLegacy";
-        public const string Oslo = "__EFMigrationsHistoryOslo";
     }
 }
