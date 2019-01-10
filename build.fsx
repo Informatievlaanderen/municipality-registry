a5efc3abaafd2717ef90feecb24dcdd5561349cb
David Cumps <david@cumps.be>
2019-01-10 15:28:49 +0100

    chore: move build description to the top

--- build.fsx
+++ build.fsx
@@ -3,6 +3,48 @@
 open Fake
 open ``Build-generic``
 
+// The buildserver passes in `BITBUCKET_BUILD_NUMBER` as an integer to version the results
+// and `BUILD_DOCKER_REGISTRY` to point to a Docker registry to push the resulting Docker images.
+
+// NpmInstall
+// Run an `npm install` to setup Commitizen and Semantic Release.
+
+// DotNetCli
+// Checks if the requested .NET Core SDK and runtime version defined in global.json are available.
+// We are pedantic about these being the exact versions to have identical builds everywhere.
+
+// Clean
+// Make sure we have a clean build directory to start with.
+
+// Restore
+// Restore dependencies for debian.8-x64 and win10-x64 using dotnet restore and Paket.
+
+// Build
+// Builds the solution in Release mode with the .NET Core SDK and runtime specified in global.json
+// It builds it platform-neutral, debian.8-x64 and win10-x64 version.
+
+// Test
+// Runs `dotnet test` against the test projects.
+
+// Publish
+// Runs a `dotnet publish` for the debian.8-x64 and win10-x64 version as a self-contained application.
+// It does this using the Release configuration.
+
+// Pack
+// Packs the solution using Paket in Release mode and places the result in the dist folder.
+// This is usually used to build documentation NuGet packages.
+
+// Containerize
+// Executes a `docker build` to package the application as a docker image. It does not use a Docker cache.
+// The result is tagged as latest and with the current version number.
+
+// DockerLogin
+// Executes `ci-docker-login.sh`, which does an aws ecr login to login to Amazon Elastic Container Registry.
+// This uses the local aws settings, make sure they are working!
+
+// Push
+// Executes `docker push` to push the built images to the registry.
+
 let dockerRepository = "municipalityregistry"
 let assemblyVersionNumber = (sprintf "2.%s")
 let nugetVersionNumber = (sprintf "%s")
@@ -70,48 +112,6 @@ Target "Publish" DoNothing
 Target "Pack" DoNothing
 Target "Containerize" DoNothing
 Target "Push" DoNothing
-
-// The buildserver passes in `BITBUCKET_BUILD_NUMBER` as an integer to version the results
-// and `BUILD_DOCKER_REGISTRY` to point to a Docker registry to push the resulting Docker images.
-
-// NpmInstall
-// Run an `npm install` to setup Commitizen and Semantic Release.
-
-// DotNetCli
-// Checks if the requested .NET Core SDK and runtime version defined in global.json are available.
-// We are pedantic about these being the exact versions to have identical builds everywhere.
-
-// Clean
-// Make sure we have a clean build directory to start with.
-
-// Restore
-// Restore dependencies for debian.8-x64 and win10-x64 using dotnet restore and Paket.
-
-// Build
-// Builds the solution in Release mode with the .NET Core SDK and runtime specified in global.json
-// It builds it platform-neutral, debian.8-x64 and win10-x64 version.
-
-// Test
-// Runs `dotnet test` against the test projects.
-
-// Publish
-// Runs a `dotnet publish` for the debian.8-x64 and win10-x64 version as a self-contained application.
-// It does this using the Release configuration.
-
-// Pack
-// Packs the solution using Paket in Release mode and places the result in the dist folder.
-// This is usually used to build documentation NuGet packages.
-
-// Containerize
-// Executes a `docker build` to package the application as a docker image. It does not use a Docker cache.
-// The result is tagged as latest and with the current version number.
-
-// DockerLogin
-// Executes `ci-docker-login.sh`, which does an aws ecr login to login to Amazon Elastic Container Registry.
-// This uses the local aws settings, make sure they are working!
-
-// Push
-// Executes `docker push` to push the built images to the registry.
 
 "NpmInstall"         ==> "Build"
 "DotNetCli"          ==> "Build"
