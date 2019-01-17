e91d2568ec758d5cea563a0da28774643791428e
David Cumps <david@cumps.be>
2019-01-17 17:01:27 +0100

    refactor: dont start when a connection string is missing

--- src/MunicipalityRegistry.Infrastructure/ContainerBuilderExtensions.cs
+++ src/MunicipalityRegistry.Infrastructure/ContainerBuilderExtensions.cs
@@ -1,5 +1,6 @@
 namespace MunicipalityRegistry.Infrastructure
 {
+    using System;
     using Be.Vlaanderen.Basisregisters.AggregateSource.SqlStreamStore.Autofac;
     using Be.Vlaanderen.Basisregisters.DataDog.Tracing.SqlStreamStore;
     using Autofac;
@@ -10,8 +11,9 @@ namespace MunicipalityRegistry.Infrastructure
         public static void RegisterEventstreamModule(this ContainerBuilder builder, IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("Events");
+
             if (string.IsNullOrWhiteSpace(connectionString))
-                return;
+                throw new ApplicationException("Missing 'Events' connectionstring.");
 
             builder
                 .RegisterModule(new SqlStreamStoreModule(connectionString, Schema.Default))
