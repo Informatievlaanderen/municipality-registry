36f19c913dd12987742346648e4a0a48bd91f187
Wouter De Rijck <wouter.derijck@kb.vlaanderen.be>
2019-03-08 15:36:28 +0100

    feat: convert lastchangedlist projections from app to lib

---
+++ src/MunicipalityRegistry.Projections.LastChangedList/LastChangedListContextMigrationFactory.cs
@@ -0,0 +1,11 @@
+namespace MunicipalityRegistry.Projections.LastChangedList
+{
+    using MigrationFactory = Be.Vlaanderen.Basisregisters.ProjectionHandling.LastChangedList.LastChangedListContextMigrationFactory;
+
+    public class LastChangedListContextMigrationFactory : MigrationFactory
+    {
+        public LastChangedListContextMigrationFactory()
+            : base("LastChangedListAdmin")
+        { }
+    }
+}
