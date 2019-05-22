214d238e52b23d2c2b1c17db609ac39ada03d291
Wouter De Rijck <wouter.derijck@kb.vlaanderen.be>
2019-05-22 14:42:30 +0200

    fix: use updated command processor in Importer

--- src/MunicipalityRegistry.Importer/NonBatchedHttpApiProxy.cs
+++ src/MunicipalityRegistry.Importer/NonBatchedHttpApiProxy.cs
@@ -1,52 +1,39 @@
+
+using Microsoft.Extensions.Logging;
+
 namespace MunicipalityRegistry.Importer
 {
-    using System;
     using System.Collections.Generic;
-    using System.Net.Http;
-    using System.Net.Http.Headers;
-    using System.Text;
     using Be.Vlaanderen.Basisregisters.GrAr.Import.Processing;
     using Be.Vlaanderen.Basisregisters.GrAr.Import.Processing.Api;
     using Newtonsoft.Json;
 
-    public class NonBatchedHttpApiProxy : IApiProxy
+    public class NonBatchedHttpApiProxy : HttpApiProxy
     {
-        private readonly IHttpApiProxyConfig _config;
-        private readonly JsonSerializer _serializer;
-
         public NonBatchedHttpApiProxy(
+            ILogger logger,
             JsonSerializer serializer,
             IHttpApiProxyConfig config)
-        {
-            _serializer = serializer;
-            _config = config;
-        }
+            : base(logger, serializer, config)
+        { }
 
-        public void ImportBatch<TKey>(IEnumerable<KeyImport<TKey>> imports)
+        public new void ImportBatch<TKey>(IEnumerable<KeyImport<TKey>> imports)
         {
-            using (var client = new HttpClient {BaseAddress = _config.BaseUrl})
+            Using(client =>
             {
-                client.Timeout = TimeSpan.FromMinutes(_config.HttpTimeoutMinutes);
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                if (!string.IsNullOrEmpty(_config.AuthUserName) &&
-                    !string.IsNullOrEmpty(_config.AuthPassword))
-                {
-                    var encodedString = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{_config.AuthUserName}:{_config.AuthPassword}"));
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", encodedString);
-                }
-
                 foreach (var import in imports)
                 foreach (var command in import.Commands)
                 {
-                    var json = _serializer.Serialize(command);
-                    var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-                    var response = client.PostAsync(_config.ImportEndpoint, content).GetAwaiter().GetResult();
-                    response.EnsureSuccessStatusCode();
+                    client
+                        .PostAsync(
+                            Config.ImportEndpoint,
+                            CreateJsonContent(Serializer.Serialize(command))
+                        )
+                        .GetAwaiter()
+                        .GetResult()
+                        .EnsureSuccessStatusCode();
                 }
-            }
+            });
         }
     }
 }
--- src/MunicipalityRegistry.Importer/NonBatchedHttpApiProxyFactory.cs
+++ src/MunicipalityRegistry.Importer/NonBatchedHttpApiProxyFactory.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Logging;
+
 namespace MunicipalityRegistry.Importer
 {
     using Be.Vlaanderen.Basisregisters.GrAr.Import.Processing.Api;
@@ -8,13 +10,15 @@ namespace MunicipalityRegistry.Importer
     {
         private readonly IHttpApiProxyConfig _config;
         private readonly JsonSerializer _serializer;
+        private readonly ILogger _logger;
 
-        public NonBatchedHttpApiProxyFactory(IHttpApiProxyConfig config)
+        public NonBatchedHttpApiProxyFactory(ILogger logger, IHttpApiProxyConfig config)
         {
+            _logger = logger;
             _config = config;
             _serializer = JsonSerializer.CreateDefault(new JsonSerializerSettings().ConfigureForCrabImports());
         }
 
-        public IApiProxy Create() => new NonBatchedHttpApiProxy(_serializer, _config);
+        public IApiProxy Create() => new NonBatchedHttpApiProxy(_logger, _serializer, _config);
     }
 }
