f56604fb06e8ceb1f6f8332a8cd615de0b810dee
David Cumps <david@cumps.be>
2019-08-21 14:38:40 +0200

    feat: use new tracing with parent span support

--- src/MunicipalityRegistry.Api.Legacy/Infrastructure/Startup.cs
+++ src/MunicipalityRegistry.Api.Legacy/Infrastructure/Startup.cs
@@ -113,12 +113,23 @@ namespace MunicipalityRegistry.Api.Legacy.Infrastructure
             StartupHelpers.CheckDatabases(healthCheckService, DatabaseTag).GetAwaiter().GetResult();
 
             app
-                .UseDatadog<Startup>(
-                    serviceProvider,
-                    loggerFactory,
-                    datadogToggle,
-                    debugDataDogToggle,
-                    _configuration["DataDog:ServiceName"])
+                .UseDataDog<Startup>(new DataDogOptions
+                {
+                    Common =
+                    {
+                        ServiceProvider = serviceProvider,
+                        LoggerFactory = loggerFactory
+                    },
+                    Toggles =
+                    {
+                        Enable = datadogToggle,
+                        Debug = debugDataDogToggle
+                    },
+                    Tracing =
+                    {
+                        ServiceName = _configuration["DataDog:ServiceName"],
+                    }
+                })
 
                 .UseDefaultForApi(new StartupUseOptions
                 {
