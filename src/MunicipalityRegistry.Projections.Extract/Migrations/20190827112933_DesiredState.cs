2268f95e37c358e845c3120fcb4924c26b8e7849
David Cumps <david@cumps.be>
2019-08-27 13:30:28 +0200

    fix: use new desiredstate columns for extract projections

---
+++ src/MunicipalityRegistry.Projections.Extract/Migrations/20190827112933_DesiredState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MunicipalityRegistry.Projections.Extract.Migrations
+{
+    public partial class DesiredState : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DesiredState",
+                schema: "MunicipalityRegistryExtract",
+                table: "ProjectionStates",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DesiredStateChangedAt",
+                schema: "MunicipalityRegistryExtract",
+                table: "ProjectionStates",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DesiredState",
+                schema: "MunicipalityRegistryExtract",
+                table: "ProjectionStates");
+
+            migrationBuilder.DropColumn(
+                name: "DesiredStateChangedAt",
+                schema: "MunicipalityRegistryExtract",
+                table: "ProjectionStates");
+        }
+    }
+}
