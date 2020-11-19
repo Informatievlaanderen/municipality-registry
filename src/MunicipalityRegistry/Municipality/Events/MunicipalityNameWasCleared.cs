6f1df6eb7df6f965b5c560271f3463449665bf86
rubcap <26146350+rubcap@users.noreply.github.com>
2020-11-19 09:38:20 +0100

    docs: changed EventDescription/added EventPropertyDescriptions GRAR-1614

--- src/MunicipalityRegistry/Municipality/Events/MunicipalityNameWasCleared.cs
+++ src/MunicipalityRegistry/Municipality/Events/MunicipalityNameWasCleared.cs
@@ -6,11 +6,16 @@ namespace MunicipalityRegistry.Municipality.Events
     using Newtonsoft.Json;
 
     [EventName("MunicipalityNameWasCleared")]
-    [EventDescription("De naam van de gemeente werd verwijderd voor een bepaalde taal.")]
+    [EventDescription("De naam van de gemeente (in een bepaalde taal) werd gewist.")]
     public class MunicipalityNameWasCleared : IHasProvenance, ISetProvenance
     {
+        [EventPropertyDescription("Interne GUID van de gemeente.")]
         public Guid MunicipalityId { get; }
+        
+        [EventPropertyDescription("Taal (voluit, EN) waarin de officiële naam staat.")]
         public Language Language { get; }
+        
+        [EventPropertyDescription("Metadata bij het event.")]
         public ProvenanceData Provenance { get; private set; }
 
         public MunicipalityNameWasCleared(
