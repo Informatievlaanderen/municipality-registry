dba944b6938afcb6210b9fe375650ae466ad40c4
Arne Dumarey <dumarey.arne@gmail.com>
2021-05-29 07:21:02 +0200

    fix: correct informatie vlaanderen to digitaal vlaanderen

--- src/MunicipalityRegistry.Api.Legacy/Municipality/Responses/MunicipalitySyndicationResponse.cs
+++ src/MunicipalityRegistry.Api.Legacy/Municipality/Responses/MunicipalitySyndicationResponse.cs
@@ -213,8 +213,8 @@ namespace MunicipalityRegistry.Api.Legacy.Municipality.Responses
     <rights>Gratis hergebruik volgens https://overheid.vlaanderen.be/sites/default/files/documenten/ict-egov/licenties/hergebruik/modellicentie_gratis_hergebruik_v1_0.html</rights>
     <updated>2020-10-15T08:48:33Z</updated>
     <author>
-        <name>agentschap Informatie Vlaanderen</name>
-        <email>informatie.vlaanderen@vlaanderen.be</email>
+        <name>Digitaal Vlaanderen</name>
+        <email>digitaal.vlaanderen@vlaanderen.be</email>
     </author>
     <link href=""https://api.basisregisters.vlaanderen.be/v1/feeds/gemeenten"" rel=""self"" />
     <link href=""https://api.basisregisters.vlaanderen.be/v1/feeds/gemeenten.atom"" rel=""alternate"" type=""application/atom+xml"" />
