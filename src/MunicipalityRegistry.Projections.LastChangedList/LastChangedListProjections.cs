ad8f0f827795ece28b4ee6cd4cde7bb50ffb79c1
Luk Vermeulen <luk.vermeulen@gmail.com>
2021-12-02 14:55:35 +0100

    feat: add oslo to lastchangedlist projection + migrate data

--- src/MunicipalityRegistry.Projections.LastChangedList/LastChangedListProjections.cs
+++ src/MunicipalityRegistry.Projections.LastChangedList/LastChangedListProjections.cs
@@ -1,5 +1,6 @@
 namespace MunicipalityRegistry.Projections.LastChangedList
 {
+    using System;
     using Be.Vlaanderen.Basisregisters.ProjectionHandling.Connector;
     using Be.Vlaanderen.Basisregisters.ProjectionHandling.LastChangedList;
     using Be.Vlaanderen.Basisregisters.ProjectionHandling.SqlStreamStore;
@@ -9,10 +10,7 @@ namespace MunicipalityRegistry.Projections.LastChangedList
     [ConnectedProjectionDescription("Projectie die markeert voor hoeveel gemeenten de gecachte data nog geüpdated moeten worden.")]
     public class LastChangedListProjections : LastChangedListConnectedProjection
     {
-        protected override string CacheKeyFormat => "legacy/municipality:{{0}}.{1}";
-        protected override string UriFormat => "/v1/gemeenten/{{0}}";
-
-        private static readonly AcceptType[] SupportedAcceptTypes = { AcceptType.Json, AcceptType.Xml };
+        private static readonly AcceptType[] SupportedAcceptTypes = { AcceptType.Json, AcceptType.Xml, AcceptType.JsonLd };
 
         public LastChangedListProjections()
             : base(SupportedAcceptTypes)
@@ -116,6 +114,29 @@ namespace MunicipalityRegistry.Projections.LastChangedList
             When<Envelope<MunicipalityWasDrawn>>(async (context, message, ct) => DoNothing());
             When<Envelope<MunicipalityNameWasImportedFromCrab>>(async (context, message, ct) => DoNothing());
             When<Envelope<MunicipalityWasImportedFromCrab>>(async (context, message, ct) => DoNothing());
+        }
+
+        protected override string BuildCacheKey(AcceptType acceptType, string identifier)
+        {
+            var shortenedAcceptType = acceptType.ToString().ToLowerInvariant();
+            return acceptType switch
+            {
+                AcceptType.Json => string.Format("legacy/municipality:{{0}}.{1}", identifier, shortenedAcceptType),
+                AcceptType.Xml => string.Format("legacy/municipality:{{0}}.{1}", identifier, shortenedAcceptType),
+                AcceptType.JsonLd => string.Format("oslo/municipality:{{0}}.{1}", identifier, shortenedAcceptType),
+                _ => throw new NotImplementedException($"Cannot build CacheKey for type {typeof(AcceptType)}")
+            };
+        }
+
+        protected override string BuildUri(AcceptType acceptType, string identifier)
+        {
+            return acceptType switch
+            {
+                AcceptType.Json => string.Format("/v1/gemeenten/{{0}}", identifier),
+                AcceptType.Xml => string.Format("/v1/gemeenten/{{0}}", identifier),
+                AcceptType.JsonLd => string.Format("/v2/gemeenten/{{0}}", identifier),
+                _ => throw new NotImplementedException($"Cannot build Uri for type {typeof(AcceptType)}")
+            };
         }
 
         private static void DoNothing() { }
