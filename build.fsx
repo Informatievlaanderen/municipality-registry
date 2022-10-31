e1b906fe4155f6970140d9ae35a92014879f9518
Arne Dumarey <dumarey.arne@gmail.com>
2022-10-31 15:22:35 +0100

    fix: test build release

--- build.fsx
+++ build.fsx
@@ -32,7 +32,6 @@ let push = push dockerRepository
 supportedRuntimeIdentifiers <- [ "msil"; "linux-x64" ]
 
 // Solution -----------------------------------------------------------------------
-
 Target.create "Restore_Solution" (fun _ -> restore "MunicipalityRegistry")
 
 Target.create "Build_Solution" (fun _ ->
