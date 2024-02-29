e7bf4ed7b38bc32f72393c78298cfa19d597d066
Arne Dumarey <dumarey.arne@gmail.com>
2024-02-29 17:06:05 +0100

    feat: remove bosa functionality GAWR-5456

--- src/MunicipalityRegistry.Projections.Legacy/Migrations/20240229160448_RemoveMunicipalityName.Designer.cs
+++ src/MunicipalityRegistry.Projections.Legacy/Migrations/20240229160448_RemoveMunicipalityName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MunicipalityRegistry.Projections.Legacy;
 
@@ -11,9 +12,10 @@ using MunicipalityRegistry.Projections.Legacy;
 namespace MunicipalityRegistry.Projections.Legacy.Migrations
 {
     [DbContext(typeof(LegacyContext))]
-    partial class LegacyContextModelSnapshot : ModelSnapshot
+    [Migration("20240229160448_RemoveMunicipalityName")]
+    partial class RemoveMunicipalityName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
