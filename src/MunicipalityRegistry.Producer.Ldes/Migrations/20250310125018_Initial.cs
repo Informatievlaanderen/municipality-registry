33a2632b6ceb091a933bf8816afdd51ea90748e6
Arne Dumarey <dumarey.arne@gmail.com>
2025-03-07 16:08:22 +0100

    feat: add Ldes Producer

---
+++ src/MunicipalityRegistry.Producer.Ldes/Migrations/20250310125018_Initial.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MunicipalityRegistry.Producer.Ldes.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "MunicipalityRegistryProducerLdes");
+
+            migrationBuilder.CreateTable(
+                name: "Municipalities",
+                schema: "MunicipalityRegistryProducerLdes",
+                columns: table => new
+                {
+                    MunicipalityId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NisCode = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    NameDutch = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NameFrench = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NameGerman = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NameEnglish = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: true),
+                    VersionTimestamp = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    FacilitiesLanguages = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OfficialLanguages = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Municipalities", x => x.MunicipalityId)
+                        .Annotation("SqlServer:Clustered", false);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectionStates",
+                schema: "MunicipalityRegistryProducerLdes",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Position = table.Column<long>(type: "bigint", nullable: false),
+                    DesiredState = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DesiredStateChangedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    ErrorMessage = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectionStates", x => x.Name);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Municipalities_NisCode",
+                schema: "MunicipalityRegistryProducerLdes",
+                table: "Municipalities",
+                column: "NisCode")
+                .Annotation("SqlServer:Clustered", true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Municipalities",
+                schema: "MunicipalityRegistryProducerLdes");
+
+            migrationBuilder.DropTable(
+                name: "ProjectionStates",
+                schema: "MunicipalityRegistryProducerLdes");
+
+            migrationBuilder.DropSchema(
+                name: "MunicipalityRegistryProducerLdes");
+        }
+    }
+}
