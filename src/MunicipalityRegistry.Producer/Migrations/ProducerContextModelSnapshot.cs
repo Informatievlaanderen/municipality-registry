3fdc2a6acc11e78f49641f1ac1ba2340891e0f84
Arne Dumarey <dumarey.arne@gmail.com>
2025-08-28 16:45:39 +0200

    fix(producer): dotnet upgrade migrations fix

--- src/MunicipalityRegistry.Producer/Migrations/ProducerContextModelSnapshot.cs
+++ src/MunicipalityRegistry.Producer/Migrations/ProducerContextModelSnapshot.cs
@@ -6,6 +6,8 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MunicipalityRegistry.Producer;
 
+#nullable disable
+
 namespace MunicipalityRegistry.Producer.Migrations
 {
     [DbContext(typeof(ProducerContext))]
@@ -15,9 +17,10 @@ namespace MunicipalityRegistry.Producer.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("ProductVersion", "5.0.6")
-                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+                .HasAnnotation("ProductVersion", "9.0.4")
+                .HasAnnotation("Relational:MaxIdentifierLength", 128);
+
+            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
             modelBuilder.Entity("Be.Vlaanderen.Basisregisters.ProjectionHandling.Runner.ProjectionStates.ProjectionStateItem", b =>
                 {
@@ -36,8 +39,7 @@ namespace MunicipalityRegistry.Producer.Migrations
                     b.Property<long>("Position")
                         .HasColumnType("bigint");
 
-                    b.HasKey("Name")
-                        .IsClustered();
+                    b.HasKey("Name");
 
                     b.ToTable("ProjectionStates", "MunicipalityRegistryProducer");
                 });
