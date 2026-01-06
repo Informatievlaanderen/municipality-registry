6c6ff763bcbef42ce1b85288412f6951dd6e1ee9
ArneD <dumarey.arne@gmail.com>
2026-01-06 16:51:54 +0100

    test: use random boolean

--- test/MunicipalityRegistry.Tests/AutoFixture/InfrastructureCustomization.cs
+++ test/MunicipalityRegistry.Tests/AutoFixture/InfrastructureCustomization.cs
@@ -7,6 +7,7 @@ namespace MunicipalityRegistry.Tests.AutoFixture
         public void Customize(IFixture fixture)
         {
             fixture.Customize(new NodaTimeCustomization());
+            fixture.Customize(new RandomBooleanSequenceCustomization());
             fixture.Customize(new SetProvenanceImplementationsCallSetProvenance());
         }
     }
